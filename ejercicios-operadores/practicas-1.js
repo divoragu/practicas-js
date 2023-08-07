f3efe4588a6e378b83ed1a6345a5a1521e552e1b
DivoRagu <103603420+divoragu@users.noreply.github.com>
2023-08-07 23:45:50 +0000

    commit

--- ejercicios-operadores/practicas-1.js
+++ ejercicios-operadores/practicas-1.js
@@ -1,2 +1,5 @@
 // ejercicio 1
-
+//1 -  Para llevar un control automatizado en la venta de boletos para ingresar a un // partido de fútbol, el Tech Leader de tu equipo te asigna la responsabilidad de crear y asignar valor a un conjunto de variables. Recordá tener presente el tipo de dato que contienen.
+//2 -  A continuación te dejamos algunos ejemplos de variables que puedes crear: Nombre - Apellido - Edad - Número de Teléfono - Socio (Valor booleano) - Fecha del partido - Hora del partido - Número de silla - Precio del boleto (con valores decimales).
+// 3. Usando el console.log(), muestra al usuario el valor asignado a cada variable.
+// 4. Finalmente, utilizando el console.log() y el typeof(), muestra
