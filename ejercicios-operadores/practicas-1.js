93c48dc2fd2545894b30e6a07a6c647480e29289
DivoRagu <103603420+divoragu@users.noreply.github.com>
2023-08-07 23:55:38 +0000

    ejercicio uno de operadores listo

--- ejercicios-operadores/practicas-1.js
+++ ejercicios-operadores/practicas-1.js
@@ -1,5 +1,33 @@
 // ejercicio 1
-//1 -  Para llevar un control automatizado en la venta de boletos para ingresar a un // partido de fútbol, el Tech Leader de tu equipo te asigna la responsabilidad de crear y asignar valor a un conjunto de variables. Recordá tener presente el tipo de dato que contienen.
-//2 -  A continuación te dejamos algunos ejemplos de variables que puedes crear: Nombre - Apellido - Edad - Número de Teléfono - Socio (Valor booleano) - Fecha del partido - Hora del partido - Número de silla - Precio del boleto (con valores decimales).
-// 3. Usando el console.log(), muestra al usuario el valor asignado a cada variable.
-// 4. Finalmente, utilizando el console.log() y el typeof(), muestra
+//1. Para llevar un control automatizado en la venta de boletos para ingresar a un partido de fútbol, el Tech Leader de tu equipo te asigna la responsabilidad de crear y asignar valor a un conjunto de variables. Recordá tener presente el tipo de dato que contienen. A continuación te dejamos algunos ejemplos de variables que puedes crear: Nombre - Apellido - Edad - Número de Teléfono - Socio (Valor booleano) - Fecha del partido - Hora del partido - Número de silla - Precio del boleto (con valores decimales).
+// 2. Usando el console.log(), muestra al usuario el valor asignado a cada variable.
+// 3. Finalmente, utilizando el console.log() y el typeof(), muestra al usuario el tipo de dato de cada una de las variables utilizadas.
+
+let nombre = "Amadeo";
+let apellido = "Ragu";
+let edad = 5;
+let numTel = 3412276177;
+let socio = true;
+let fechaPartido = "12-08-2023";
+let horaPartido = "20:00";
+let numSilla = 23;
+let precioBoleto = 2000;
+
+console.log(nombre)
+console.log(typeof(nombre))
+console.log(apellido)
+console.log(typeof(apellido))
+console.log(edad)
+console.log(typeof(edad))
+console.log(numTel)
+console.log(typeof(numTel))
+console.log(socio)
+console.log(typeof(socio))
+console.log(fechaPartido)
+console.log(typeof(fechaPartido))
+console.log(horaPartido)
+console.log(typeof(horaPartido))
+console.log(numSilla)
+console.log(typeof(numSilla))
+console.log(precioBoleto)
+console.log(typeof(precioBoleto))
