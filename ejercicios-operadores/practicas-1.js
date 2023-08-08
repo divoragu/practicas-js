299985403bfe6479ddf03e6adeacd65a63b5a2e4
DivoRagu <103603420+divoragu@users.noreply.github.com>
2023-08-08 00:14:46 +0000

    ejercicio a resolver 2

--- ejercicios-operadores/practicas-1.js
+++ ejercicios-operadores/practicas-1.js
@@ -31,3 +31,24 @@ console.log(numSilla)
 console.log(typeof(numSilla))
 console.log(precioBoleto)
 console.log(typeof(precioBoleto))
+
+
+//ejercicio 2
+// En el archivo se desarrolla un código para calcular el nuevo sueldo a un trabajador, para ello debes declarar las siguientes variables y almacenar en ellas los siguientes datos del trabajador: 
+// a. nombre
+// b. apellido
+// c. sueldoActual
+// d. porcentajeAumento
+// 3. Almacenar en una variable el “calculoAumento”. Esta contendrá el resultado de la operación del aumento del trabajador (sueldoActual * porcentajeAumento)/ 100).
+// 4. En otra variable “nuevoSueldo”, almacena el resultado del cálculo del nuevo
+// sueldo.
+// 5. Usando el console.log() y concatenando string, muestra en la consola de Visual
+// Studio Code los contenidos de cada una de las variables:
+// Ejemplo:
+
+// Hola, estimad@ Gloria Medina
+// En base a su sueldo actual:
+// $20000
+// Ha recibido un aumento del 25%:
+// $5000
+// y su nuevo sueldo es de: $25000
